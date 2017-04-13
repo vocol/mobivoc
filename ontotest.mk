e77b0772f221b54de65a0d15b9d05daf430130bd
Sebastian Tramp <sebastian.tramp@eccenca.com>
2017-04-13 09:42:02 +0200

    add intial test config file

---
+++ ontotest.mk
@@ -0,0 +1,18 @@
+#defined schemas and data files you want to test as filenames separated by whitespace
+#schemas are ontology files, instances contain individuals instantiating ontology resources
+#manualtests in form of RDFUnit testcase definitions can be added as well
+#MANUALSRC=./includes/rdfunit-owl-tests.ttl
+#INSTANCESRC=test-test-cases.ttl
+##include files which should be used for widoco generation
+##DOCSRC=test-test-cases.ttl
+SCHEMASRC=index.ttl index-head.ttl ChargingPoints.ttl Parking.ttl
+
+#disable manual and/or automatic tests. default: don't skip tests (=false)
+#SKIPAUTOTESTS=true
+#SKIPMANUALTESTS=true
+
+# the dataplatform where "make publish" will publish the schema graph
+SCHEMASRC_PUBLISH_DP=https://vocab.eccenca.com/
+# the graph into which "make publish" will publish the schema graph
+SCHEMASRC_PUBLISH_GRAPH=http://schema.mobivoc.org/
+
